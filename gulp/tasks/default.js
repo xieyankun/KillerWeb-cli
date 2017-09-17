23ad33b3d07a4fa91f2478e2a78f50aa102c2d39
ykxie <ykxie@kaikeba.com>
2017-09-17 09:51:54 +0800

    add image

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['html', 'sass', 'script', 'webserver', 'watch']);
+gulp.task('default', ['html', 'image', 'sass', 'script', 'webserver', 'watch']);
